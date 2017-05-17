d1ae32d49135c771be4b24879cb3797ed371273b
장조성 <utopiazn@naver.com>
2017-05-17 22:14:43 +0900

    예졔8 확인 필요

---
+++ SpringEx/src/kh02/spring/Example008/controller/GameInfoController.java
@@ -0,0 +1,18 @@
+package kh02.spring.Example008.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GameInfoController {
+
+	@RequestMapping("/game/info")
+	public String gameInfo() {
+		return "game/users/info";
+	}
+	
+	@RequestMapping("/game/list")
+	public String gameList() {
+		return "game/list";
+	}
+}
