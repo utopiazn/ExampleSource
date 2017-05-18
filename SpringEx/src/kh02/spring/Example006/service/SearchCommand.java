f13c0a204b2b266ab9eb0cf7d6ccf941cefafeef
KH_I\user1 <utopiazn@naver.com>
2017-05-18 11:37:06 +0900

    dddd

---
+++ SpringEx/src/kh02/spring/Example006/service/SearchCommand.java
@@ -0,0 +1,33 @@
+package kh02.spring.Example006.service;
+
+public class SearchCommand {
+	
+	private String type;
+	private String query;
+	private int page;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		
+		
+		
+		this.type = type;
+	}
+	public String getQuery() {
+		return query;
+	}
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	
+	
+	
+
+}
