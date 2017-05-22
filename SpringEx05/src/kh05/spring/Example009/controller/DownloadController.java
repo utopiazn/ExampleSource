f3161a9c70ef83f10f0dd3b846dd114f2a4d5d08
장조성 <utopiazn@naver.com>
2017-05-22 17:35:59 +0900

    예제 관련 소스 전부 추가함. ㅡ,ㅡㅋ

---
+++ SpringEx05/src/kh05/spring/Example009/controller/DownloadController.java
@@ -0,0 +1,39 @@
+package kh05.spring.Example009.controller;
+
+import java.io.File;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DownloadController implements ApplicationContextAware {
+
+	private WebApplicationContext context = null;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+		
+		this.context =(WebApplicationContext)applicationContext;
+	}
+	
+	@RequestMapping("/file")
+	public ModelAndView download() throws Exception {
+		
+		File downloadFile = getFile();
+		return new ModelAndView("download", "downloadFile", downloadFile);
+	
+	}
+
+	private File getFile() {
+		String path = context.getServletContext().getRealPath(
+				"/WEB-INF/설명.txt");
+		return new File(path);
+	}
+
+}
---
+++ SpringEx05/src/kh05/spring/Example013/controller/PageReportController.java
@@ -0,0 +1,26 @@
+package kh05.spring.Example013.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class PageReportController {
+
+	
+	@RequestMapping("/pageJsonReport")
+	public ModelAndView jsonReport() {
+		List<PageRank> pageRanks = new ArrayList<PageRank>();
+		pageRanks.add(new PageRank(1, "/bbs/mir2/list"));
+		pageRanks.add(new PageRank(2, "/bbs/mir3/list"));
+		pageRanks.add(new PageRank(3, "/bbs/changchun2/list"));
+		return new ModelAndView("pageJsonReport", "report", new PageRankReport(
+				pageRanks));
+	}
+
+}
