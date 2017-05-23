272058282a83569106e306aa5b84df353b72e59b
KH_I\user1 <utopiazn@naver.com>
2017-05-23 13:12:51 +0900

    no message

--- SpringShop01/src/sql/3-1.sql
+++ SpringShop01/src/sql/3-1.sql
@@ -11,9 +11,10 @@ CREATE SEQUENCE ITEM_ID_SEQ;
 
 DELETE FROM ITEM;
 
-INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'����',50,'���� ���Ե� �������� �Ƿ�ȸ���� �����ϴ�. ��Ÿ�� C�� ǳ���մϴ�.','lemon.jpg');
-INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'������',100,'��Ÿ�� C�� ǳ���մϴ�. ������ �ֽ��� ���ø� �����ϴ�.','orange.jpg');
-INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'Ű��',200,'��Ÿ�� C�� �ſ� ǳ���մϴ�. ���̾�Ʈ�� �̿뿡 �����ϴ�.','kiui.jpg');
-INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'����',300,'��������� �ٷ� �����ϰ� �־� �׻�ȭ �ۿ��� �մϴ�.' ,'budou.jpg');
-INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'����',800,'��Ÿ�� C�� �ö󺸳��̵带 �ٷ� �����ϰ� �ֽ��ϴ�.','ichigo.jpg');
-INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'��',1000,'�ó��Ǹ��� �����ϰ� �־� ���� ���濡 ���ٰ� �մϴ�.','mikan.jpg');
+
+INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'레몬',50,'레몬에 포함된 구연산은 피로회복에 좋습니다. 비타민 C도 풍부합니다.','lemon.jpg');
+INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'오렌지',100,'비타민 C가 풍부합니다. 생과일 주스로 마시면 좋습니다.','orange.jpg');
+INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'키위',200,'비타민 C가 매우 풍부합니다. 다이어트나 미용에 좋습니다.','kiui.jpg');
+INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'포도',300,'폴리페놀을 다량 함유하고 있어 항산화 작용을 합니다.' ,'budou.jpg');
+INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'딸기',800,'비타민 C나 플라보노이드를 다량 함유하고 있습니다.','ichigo.jpg');
+INSERT INTO ITEM VALUES(ITEM_ID_SEQ.NEXTVAL,'귤',1000,'시네피린을 함유하고 있어 감기 예방에 좋다고 합니다.','mikan.jpg');
